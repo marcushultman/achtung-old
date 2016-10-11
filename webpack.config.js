629a4978ff4410c70c1749d4c5644a4a9c241458
stroem <jonathan.strom@gmail.com>
2016-10-11 21:07:02 +0200

    Cleaning deps

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,29 @@
 var path = require('path')
 
 module.exports = {
-  entry: {
-    app: [ './src/main.js' ]
-  },
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: '[name].js',
-    chunkFilename: '[id].js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js/,
-        include: /src/,
-        loader: 'babel',
-        query: {
-          plugins: [ 'transform-runtime' ],
-          presets: [ 'es2015' ]
-        }
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['', '.js'],
-    moduleDirectories: ['node_modules']
-  },
-  plugins: []
+    entry: {
+        app: [ './src/main.js' ]
+    },
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: '[name].js',
+        chunkFilename: '[id].js'
+    },
+    module: {
+        loaders: [{
+                test: /\.js/,
+                include: /src/,
+                loader: 'babel',
+                query: {
+                    plugins: ['transform-runtime'],
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js'],
+        moduleDirectories: ['node_modules']
+    },
+    plugins: []
 }
