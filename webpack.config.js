fee69d7636d439e3836bbf183745ce2ec4d3fb60
stroem <jonathan.strom@gmail.com>
2016-10-11 21:22:38 +0200

    Skeleton for game classes

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
 var path = require('path')
 
 module.exports = {
-    entry: {
-        app: [ './lib/main.js' ]
-    },
-    output: {
-        path: path.join(__dirname, 'build'),
-        filename: '[name].js',
-        chunkFilename: '[id].js'
-    },
-    module: {
-        loaders: [{
-                test: /\.js/,
-                include: /src/,
-                loader: 'babel',
-                query: {
-                    plugins: ['transform-runtime'],
-                    presets: ['es2015']
-                }
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['', '.js'],
-        moduleDirectories: ['node_modules']
-    },
-    plugins: []
+  entry: {
+    app: [ './lib/main.js' ]
+  },
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: '[name].js',
+    chunkFilename: '[id].js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js/,
+      include: /src/,
+      loader: 'babel',
+      query: {
+        plugins: ['transform-runtime'],
+        presets: ['es2015']
+      }
+    }
+  ]
+},
+resolve: {
+  extensions: ['', '.js'],
+  moduleDirectories: ['node_modules']
+},
+plugins: []
 }
